6d40a2fc6fa08836fa104ecc18dae332cc595467
Alexandritax <silvasolisdelbarrio@gmail.com>
2021-10-19 16:23:29 -0500

    implementar los grafos de baja y alta complejidad.

--- codigo.py
+++ codigo.py
@@ -1,9 +1,10 @@
-import sys
+import sys #permite usar la funcion maxsize para representar un numero enorme.
 import concurrent.futures
 
 
 #definimos como fuente el nodo incial del grafo
 #los otros nodos los llamare vertices
+#https://www.geeksforgeeks.org/python-program-for-dijkstras-shortest-path-algorithm-greedy-algo-7/
 
 class Graph():
     def __init__(self,vertices): #inicializando con los vertices
@@ -12,18 +13,19 @@ class Graph():
         for lineas in range(vertices)]] #queremos una matriz cuadrada
 
     def printSolucion(self,dist,src): #dist es la lista de distancias
-        print("Distancia de los vertices desde el nodo {}".format(src))
+        print("\tDistancia a los vertices desde el nodo {}".format(src))
         for node in range(self.V):
-            print("router" ,node, "--> ", dist[node])
+            if(node != src):
+                print("\trouter" ,node, "--> ", dist[node])
         print("\n")
     
     def minDistance(self,dist,sptSet):
-        min = sys.maxsize
+        min = sys.maxsize #peudo-infinito
         
         for v in range(self.V):
-            if dist[v]< min and sptSet[v] == False:
+            if dist[v]< min and sptSet[v] == False: #si el nodo no fue visitado y la distancia sea menor al minimo.
                 min = dist[v]
-                min_index = v
+                min_index = v #retorna el nodo con la menor distancia
 
         return min_index
     
@@ -34,20 +36,27 @@ class Graph():
 
         for _ in range(self.V):
 
-            u = self.minDistance(dist, sptSet)
+            u = self.minDistance(dist, sptSet) # u sera el nodo con la menor distancias
+            # Primero u sera el nodo fuente, dado que su distancia es 0
             
             sptSet[u] = True
+            #se marca quien es el nodo fuente.
 
             for v in range(self.V):
-                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[u] + self.graph[u][v] < dist[v]: #buscara la distancia de los demas nodos desde el nodo fuente
+                    #self.graph[u][v] > 0 una arista desde el nodo u y v exite si en el grafo tiene un valor mayor a 0 su distancia
+                    # sptSet[v]==false no queremos trabajar con el nodo de origen.
+                    # dist[u] + self.graph[u][v] < dist[v] busca que la nueva distancia sea menor a la distancia anterior.
                     dist[v] = dist[u] + self.graph[u][v]
 
-        self.printSolucion(dist,src)
+        self.printSolucion(dist,src) #imprime las distancias
 
 
 def main():
-    g = Graph(9)
-    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+    print("Grafo de baja complejidad:\n")
+    nodos_LC = 9
+    Low_complexity = Graph(nodos)
+    Low_complexity.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
            [4, 0, 8, 0, 0, 0, 0, 11, 0],
            [0, 8, 0, 7, 0, 4, 0, 0, 2],
            [0, 0, 7, 0, 9, 14, 0, 0, 0],
@@ -58,9 +67,12 @@ def main():
            [0, 0, 2, 0, 0, 0, 6, 7, 0]
            ]
  
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        nodes = [i for i in range(9)]
-        executor.map(g.dijkstra,nodes)
+    with concurrent.futures.ProcessPoolExecutor() as executor: #crea un ejecutor de multi-procesos
+        nodes = [i for i in range(nodos_LC)] 
+        executor.map(Low_complexity.dijkstra,nodes) #ejecuta dijkstra en varios procesos para todos los nodos paralelamente.
+    print("---------------------------------------------------\n")
+    print("Grafo de alta complejidad:\n")
+    nodos_HC = 12
 
 if __name__ == "__main__":
     main()
